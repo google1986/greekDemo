e258f3a281fbc5786ca47547b3ba3baa77707e1b
gulj <google_zw@163.com>
2018-09-27 20:12:47 +0800

    first commit

---
+++ src/main/java/com/htzw/study/GreekApplication.java
@@ -0,0 +1,12 @@
+package com.htzw.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreekApplication.class, args);
+	}
+}
