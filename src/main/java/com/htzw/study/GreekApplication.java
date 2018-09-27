9e42473614f1aa464e02a0e99b213d2f25411688
gulj <google_zw@163.com>
2018-09-27 20:28:57 +0800

    delete application.properties

--- src/main/java/com/htzw/study/GreekApplication.java
+++ src/main/java/com/htzw/study/GreekApplication.java
@@ -3,6 +3,9 @@ package com.htzw.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class GreekApplication {
 
