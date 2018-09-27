d658fbbef243074867f3459fbd589d3f31fe6052
gulj <google_zw@163.com>
2018-09-27 20:40:01 +0800

    修改上下文配置

---
+++ src/main/java/com/htzw/study/controller/GreekController.java
@@ -0,0 +1,17 @@
+package com.htzw.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/9/27.
+ * @author glj
+ */
+@RestController
+public class GreekController {
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public String hello(){
+        return "Hello Greek!";
+    }
+}
